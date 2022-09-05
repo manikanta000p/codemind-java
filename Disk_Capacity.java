c3bf16a2c251672848a69b01441ca17cc0f5f767
manikanta000p <85338506+manikanta000p@users.noreply.github.com>
2022-09-05 21:08:10 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner sc=new Scanner (System.in);         int a,b,c,x;         a=sc.nextInt();         b=sc.nextInt();         c=sc.nextInt();         x=2*a*b*c*512;         System.out.println(x);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner (System.in);
+        int a,b,c,x;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        x=2*a*b*c*512;
+        System.out.println(x);
+    }
+}
